258abc7abf4964b965e32bc8ecb406b34ceebfb1
qachicago <114437306+qachicago@users.noreply.github.com>
2023-01-11 22:32:52 -0500

    //ten is merged from slave_2

---
+++ src/Eight.java
@@ -0,0 +1,6 @@
+public class Eight {
+    public static void main(String[] args) {
+        //some code here from Alex
+
+    }
+}
---
+++ src/ten.java
@@ -0,0 +1,6 @@
+public class ten {
+    public static void main(String[] args) {
+        //page 10 form slave_2
+        //ten is merged from slave_2
+    }
+}
